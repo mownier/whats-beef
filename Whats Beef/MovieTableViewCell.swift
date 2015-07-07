393344854e87bf542c2c7090281f9301a4b42e91
Mounir Ybanez <rinuom91@gmail.com>
2015-07-07 13:49:46 +0800

    Added table view cell for displaying each movie.

--- Whats Beef/MovieTableViewCell.swift
+++ Whats Beef/MovieTableViewCell.swift
@@ -6,8 +6,19 @@
 //  Copyright (c) 2015 iOS applicant. All rights reserved.
 //
 
-import Cocoa
-
 class MovieTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var channelImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.channelImageView.layer.borderWidth = 0
+        self.channelImageView.layer.borderColor = UIColor.grayColor().CGColor
+        self.channelImageView.backgroundColor = UIColor.lightGrayColor()
+        self.channelImageView.layer.cornerRadius = self.channelImageView.frame.size.width / 2
+    }
 }
