f3c0686d7a99694b44d73899ff7cea51258ae397
iOS applicant <iOSapplicant@Freelancers-iMac-2.local>
2015-07-07 09:58:32 +0800

    Initial Commit

---
+++ Whats Beef/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats Beef
+//
+//  Created by iOS applicant on 7/07/2015.
+//  Copyright (c) 2015 iOS applicant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
